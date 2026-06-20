0e8bb110152d6a98fbec44548b586a5caf207dcd
Mahmood Ansari <60201069+Mansarix@users.noreply.github.com>
2026-06-20 11:16:15 +0330

    add contract

---
+++ Mansari.Store.Contracts/Abstraction/IIntegrationEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace Mansari.Store.Contracts.Abstractions;
+
+public interface IIntegrationEvent
+{
+    Guid Id { get; }
+    Guid CorrelationId { get; }
+    DateTime OccurredOnUtc { get; }
+}
---
+++ Mansari.Store.Contracts/Order/OrderCreatedEvent.cs
@@ -0,0 +1,15 @@
+﻿using Mansari.Store.Contracts.Abstractions;
+
+namespace Mansari.Store.Contracts.Orders;
+
+public sealed record OrderCreatedEvent(
+    Guid OrderId,
+    Guid BookId,
+    int Quantity,
+    Guid CorrelationId
+) : IIntegrationEvent
+{
+    public Guid Id { get; init; } = Guid.NewGuid();
+
+    public DateTime OccurredOnUtc { get; init; } = DateTime.UtcNow;
+}
